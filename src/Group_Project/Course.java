36576e3efdfaae773c62d2db18fd0f8de36d320c
jmitche3 <jmitche3@unb.ca>
2019-03-24 19:39:23 -0300

    Modifying my code to use Spencer's classes

--- src/Group_Project/Course.java
+++ src/Group_Project/Course.java
@@ -1,5 +1,44 @@
 package Group_Project;
 
 public class Course {
+	final private String courseCode;
+	final private String courseName;
+    final private Double courseCredit;
+	
+	public Course(String courseCodei, String sectionNumberi, String courseNamei, Double courseCrediti) {
+		this.courseCode = courseCodei;
+		this.courseName = courseNamei;
+        this.courseCredit = courseCrediti;
+		
+	}
+
+	/**
+	 * @return the courseCode
+	 */
+	public String getCourseCode() {
+		return courseCode;
+	}
+
+	/**
+	 * @return the courseName
+	 */
+	public String getCourseName() {
+		return courseName;
+	}
+
+	/**
+	 * @return the courseCredit
+	 */
+	public Double getCourseCredit() {
+		return courseCredit;
+    }
+
+    /**
+     * @return the object as a string
+     */
+	public String toString() {
+		return (courseCode +" " +courseName +" " +courseCredit);
+	}
+	
 
 }
